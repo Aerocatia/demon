5417934ea1d7adc3af6726b0fd5f177208a6f8c6
Butterfree <butterfree@localhost>
2025-01-09 00:06:54 +0100

    Fix build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,20 @@
 fn main() {
     // We are only interested in building this for i686-pc-windows-* targets. Don't try to run
     // windres for anything else.
-    let Ok(target) = std::env::var("CARGO_BUILD_TARGET") else {
+    let Ok(arch) = std::env::var("CARGO_CFG_TARGET_ARCH") else {
+        println!("cargo:warning=Can't detect target arch; windres won't be called");
         return
     };
-    if target.starts_with("i686-pc-windows-") {
+    let Ok(os) = std::env::var("CARGO_CFG_TARGET_OS") else {
+        println!("cargo:warning=Can't detect target OS; windres won't be called");
+        return
+    };
+    if arch == "x86" && os == "windows" {
         embed_resource::compile("strings/custom_edition/strings.rc", embed_resource::NONE)
             .manifest_optional()
             .expect("Failed to run windres; this is needed to embed the strings.dll contents!");
+    }
+    else {
+        println!("cargo:warning=Not building for i686-pc-windows-* (target_arch={arch}, target_os={os}); windres won't be called");
     }
 }
