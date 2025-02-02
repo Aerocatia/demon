19d81756419cee52f162f7ef8a5d755ae9d2fb20
Butterfree <butterfree@localhost>
2025-02-02 21:35:08 +0100

    Adjust base ratio
    B?

--- src/input/mouse.rs
+++ src/input/mouse.rs
@@ -1,20 +1,21 @@
 use c_mine::c_mine;
 
-/// number of "dots" for a full rotation
-pub const BASE_AIM_DOT_RATIO: f32 = 1.0 / 512.0;
-
 /// if >0, override mouse sensitivity
 pub static mut MOUSE_SENSITIVITY: f32 = 0.0;
+
+pub const BASE_RATIO: f64 = 0.4 * 0.00075;
 
 #[c_mine]
 pub extern "C" fn mouse_sensitivity(sensitivity: f32, raw_input: i32) -> f32 {
     let sensitivity_override = unsafe { MOUSE_SENSITIVITY };
 
-    if sensitivity_override > 0.0 {
-        sensitivity_override * (raw_input as f32) * BASE_AIM_DOT_RATIO
+    let delta = if sensitivity_override > 0.0 {
+        sensitivity_override as f64 * (raw_input as f64) * BASE_RATIO
     }
     else {
-        sensitivity * (raw_input as f32) * BASE_AIM_DOT_RATIO
-    }
+        sensitivity as f64 * (raw_input as f64) * BASE_RATIO
+    };
+
+    delta as f32
 }
 
