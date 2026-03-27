0080628b87a86346644017dd4d7e740c4de6cea8
Psyduck <psyduck@localhost>
2026-03-27 19:39:20 +0100

    Add an option to nop; pass function name to error_function
    Allocates a 256 KiB buffer to store the function names, as we won't have
    access to those function names once we're done with our setup.
    
    Psy~ <3

--- utils/scythunk/src/lib.rs
+++ utils/scythunk/src/lib.rs
@@ -7,9 +7,9 @@ mod util;
 use alloc::{collections::btree_map::BTreeMap, ffi::CString, string::String};
 use min32::dllmain;
 use util::messagebox;
-use windows_sys::Win32::System::{LibraryLoader::{GetProcAddress, LoadLibraryA}, Memory::{PAGE_EXECUTE_READWRITE, VirtualProtect}, SystemServices};
+use windows_sys::Win32::System::{LibraryLoader::{GetProcAddress, LoadLibraryA}, Memory::{MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE, VirtualAlloc, VirtualProtect}, SystemServices};
 
-use core::{ffi::{CStr, c_void}, mem::transmute};
+use core::{ffi::{CStr, c_char, c_void}, mem::transmute};
 
 use crate::util::terminate;
 
@@ -108,6 +108,18 @@ unsafe fn attach() {
         terminate();
     };
 
+    // We want to allocate as far away from 0x40440000 as possible to avoid conflicts.
+    //
+    // This will hold nopped function names which we'll pass into error_function.
+    let jmp_data_len = 256 * 1024;
+    let jmp_data = VirtualAlloc(0xCACA0000 as *mut _, jmp_data_len, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if jmp_data.is_null() {
+        messagebox("Failed to load", "Can't allocate 256 KiB @ 0xCACA0000.\n\nIs your EXE not Large Address Aware? Or maybe stuff is already there...");
+        terminate();
+    }
+    let jmp_data = core::slice::from_raw_parts_mut(jmp_data as *mut u8, jmp_data_len);
+    let mut remaining_jmp_data = jmp_data;
+
     let functions: BTreeMap<String, Replacement> = match serde_json::from_str(all_functions) {
         Ok(n) => n,
         Err(e) => {
@@ -145,9 +157,45 @@ unsafe fn attach() {
 
         let function_name_cstr = CString::new(function_name.clone()).expect("no way this can fail");
 
-        let address_of_function = match replacement_data.disabled {
-            Some(true) => error_function as *const u8 as usize,
-            _ => {
+        let address_of_function = match replacement_data.disable {
+            Some(DisableType::Forbid) => {
+                let Some((name, remaining)) = remaining_jmp_data.split_at_mut_checked(function_name_cstr.count_bytes() + 1) else {
+                    messagebox("Failed to load", &alloc::format!("Ran out of jmp buffer space to write the function name."));
+                    terminate();
+                };
+
+                name.copy_from_slice(function_name_cstr.as_bytes_with_nul());
+
+                // push NAME ; 5 bytes
+                // call error_function ; 5 bytes
+                // int3 ; 1 byte
+                // = 11 bytes
+
+                let Some((asm_code, remaining)) = remaining.split_at_mut_checked(11) else {
+                    messagebox("Failed to load", &alloc::format!("Ran out of jmp buffer space to write the error function handler."));
+                    terminate();
+                };
+
+                let function_name_ptr = name.as_ptr();
+
+                asm_code[0x0] = 0x68; // push...
+                asm_code[0x1..0x5].copy_from_slice(&(function_name_ptr as usize).to_le_bytes());
+
+                asm_code[0x5] = 0xE8; // call...
+                let offset = asm_code[0xA..].as_ptr() as usize;
+                asm_code[0x6..0xA].copy_from_slice(&((error_function as *const () as usize).wrapping_sub(offset)).to_le_bytes());
+
+                asm_code[0xA] = 0xCC;
+
+                // Rest of data goes here then
+                remaining_jmp_data = remaining;
+                
+                asm_code.as_ptr() as *const () as usize
+            },
+            Some(DisableType::Nop) => {
+                nop_function as *const () as usize
+            },
+            None => {
                 let Some(proc_addr) = GetProcAddress(demon, function_name_cstr.as_ptr() as *const u8) else {
                     messagebox("Failed to load", &alloc::format!("Function {function_name} has does not exist in demon.dll."));
                     terminate();
@@ -155,6 +203,7 @@ unsafe fn attach() {
                 proc_addr as usize
             }
         };
+
 
         let thunk = &mut *thunk_addr;
         let mut old_protect = 0;
@@ -190,13 +239,30 @@ unsafe fn attach() {
 #[derive(serde::Deserialize)]
 struct Replacement {
     address: String,
-    disabled: Option<bool>,
+    disable: Option<DisableType>,
     ignore: Option<bool>,
     sudo: Option<bool>
 }
 
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum DisableType {
+    Forbid,
+    Nop
+}
+
 #[unsafe(no_mangle)]
-pub extern "C" fn error_function() {
-    messagebox("Disabled function called", &alloc::format!("SCYTHER!! >w< *uses Cut on your process*"));
+pub unsafe extern "C" fn error_function(function: *const c_char) {
+    let f = if function.is_null() {
+        "??? (null function passed lol)"
+    }
+    else {
+        CStr::from_ptr(function).to_str().unwrap_or("non-utf8 function name")
+    };
+
+    messagebox("Forbidden function called", &alloc::format!("SCYTHER!! >w< *uses Cut on your process*\n\nTried to call '{f}' which is marked as forbidden.\n\nThis function can't be used. It's recommended that this function is forgotten. Once forgotten, this function can't be called."));
     terminate();
 }
+
+#[unsafe(no_mangle)]
+pub extern "C" fn nop_function() -> usize { 0 }
