209e19cdfa0be4b97bbaab78979409fb7bad0ab5
Aerocatia <aerocatia@tutanota.com>
2026-04-05 10:24:00 +1200

    assert the sizes for (unicode) string list tags

--- source/text/text_group.h
+++ source/text/text_group.h
@@ -14,10 +14,12 @@ enum {
 struct string_list_string_reference {
     struct tag_data string;
 };
+static_assert(sizeof(struct string_list_string_reference) == 20);
 
 struct string_list_group_header {
     struct tag_block string_references;
 };
+static_assert(sizeof(struct string_list_group_header) == 12);
 
 #define string_list_get_header(index) TAG_GET(STRING_LISTS_GROUP_TAG, (index), struct string_list_group_header)
 #define string_list_get_string_reference(header, index) (TAG_BLOCK_GET_ELEMENT(&(header)->string_references, (index), struct string_list_string_reference))
@@ -27,10 +29,12 @@ const char *string_list_get_string(int32_t tag_index, int16_t string_index);
 struct unicode_string_list_string_reference {
     struct tag_data string;
 };
+static_assert(sizeof(struct unicode_string_list_string_reference) == 20);
 
 struct unicode_string_list_group_header {
     struct tag_block string_references;
 };
+static_assert(sizeof(struct unicode_string_list_group_header) == 12);
 
 #define unicode_string_list_get_header(index) TAG_GET(UNICODE_STRING_LISTS_GROUP_TAG, (index), struct unicode_string_list_group_header)
 #define unicode_string_list_get_string_reference(header, index) (TAG_BLOCK_GET_ELEMENT(&(header)->string_references, (index), struct unicode_string_list_string_reference))
